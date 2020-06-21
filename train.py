88b65582d59826d8fd913d7531f11d4d28056d7a
ben <pingheng.chang@gmail.com>
2020-06-21 18:30:29 +0800

    testing read data

--- train.py
+++ train.py
@@ -36,7 +36,13 @@ def main():
     batch_size = 32
     validation_size = .16
 
+    train_path = 'done_dataset/train/'
+    test_path = 'done_dataset/test/'
+    checkpoint_dir = "models/"
 
+    data = dataset.read_train_sets(train_path, img_size, classes, validation_size=validation_size)
+    test_images, test_ids = dataset.read_test_set(test_path, img_size)
+    
     classes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',\
           '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',\
           '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', \
